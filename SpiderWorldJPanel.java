4d60cd6466bbf4a6087c0db4140925c60d86fb82
Yud16 <89806105+Yud16@users.noreply.github.com>
2024-02-01 13:10:13 -0800

    drawing blocks in SpideWorldPanel

--- SpiderWorldJPanel.java
+++ SpiderWorldJPanel.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+import java.util.Map;
 
 class SpiderPanel extends JPanel {
-
+    private int gridCellSize = 75;
+    private int gridX = 20;
+    private int gridY = 200;
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -11,9 +15,7 @@ class SpiderPanel extends JPanel {
         int panelHeight = getHeight();
 
         // Draw 5x5 grid with gray outline
-        int gridCellSize = 75;
-        int gridX = 0;
-        int gridY = 200;
+
         for (int row = 0; row < 5; row++) {
             for (int col = 0; col < 5; col++) {
                 int x = gridX + col * gridCellSize;
@@ -34,6 +36,15 @@ class SpiderPanel extends JPanel {
         g.setColor(Color.BLACK);
         g.drawString("Spider World", 10, 20);
     }
+    public int getGridX() {
+        return gridX;
+    }
+    public int getGridCellSize() {
+        return gridCellSize;
+    }
+    public int getGridY() {
+        return gridY;
+    }
 }
 
 public class SpiderWorldJPanel {
@@ -41,9 +52,56 @@ public class SpiderWorldJPanel {
         JFrame frame = new JFrame("Spider World");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(5000, 5000);
-
+        //create window for game
         SpiderPanel spiderPanel = new SpiderPanel();
         frame.add(spiderPanel);
+
+        //draw blocks with text
+        DrawBlocks blocks = new DrawBlocks();
+        //****draw step, turn, and paint block****
+        //****draw the 4 blocks with color up top****
+
+        /*load level info
+        String levelFile = "path to txt file with level";
+        Map<String, Object> spiderWorldInfo = LoadInfo.readSpiderWorldInfo(levelFile);
+        int[]spider_loc = LoadInfo.getSpiderLocation(spiderWorldInfo);
+        int num_diamonds = LoadInfo.getNumDiamonds(spiderWorldInfo);
+        int curr_level = LoadInfo.getCurrentLevel(spiderWorldInfo);
+        List<int[]> diamond_locs = LoadInfo.getDiamondLocations(spiderWorldInfo);
+         */
+
+        /*draw spider
+        int x = spiderPanel.getGridX() + spider_loc[0] * spiderPanel.getGridCellSize();
+        int y = spiderPanel.getGridY() + spider_loc[1] * spiderPanel.getGridCellSize();
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, gridCellSize, gridCellSize);
+        */
+
+        /*draw red diamonds
+        for (int i = 0; i < num_red_diamonds; i++) {
+            int x = spiderPanel.getGridX() + red_diamond_locs[i][0] * spiderPanel.getGridCellSize();
+            int y = spiderPanel.getGridY() + red_diamond_locs[i][1] * spiderPanel.getGridCellSize();
+            g.setColor(Color.RED);
+            g.fillRect(x, y, gridCellSize, gridCellSize);
+        }*/
+        /*draw blue diamonds
+        for (int i = 0; i < num_red_diamonds; i++) {
+            int x = spiderPanel.getGridX() + red_diamond_locs[i][0] * spiderPanel.getGridCellSize();
+            int y = spiderPanel.getGridY() + red_diamond_locs[i][1] * spiderPanel.getGridCellSize();
+            g.setColor(Color.BLUE);
+            g.fillRect(x, y, gridCellSize, gridCellSize);
+        }*/
+        /*draw green diamonds
+        for (int i = 0; i < num_red_diamonds; i++) {
+            int x = spiderPanel.getGridX() + red_diamond_locs[i][0] * spiderPanel.getGridCellSize();
+            int y = spiderPanel.getGridY() + red_diamond_locs[i][1] * spiderPanel.getGridCellSize();
+            g.setColor(Color.GREEN);
+            g.fillRect(x, y, gridCellSize, gridCellSize);
+        }*/
+
+        /*drag and drop
+        DragDrop d = new DragDrop();
+        frame.add(d);*/
 
         frame.setVisible(true);
     }
